7757a51f1c6bb7be8baef9301f420468b1f126ea
karl kalvik <karl@domos.no>
2019-02-18 09:52:42 +0100

    Button to flipp monster card

--- Game/Monster.gd
+++ Game/Monster.gd
@@ -7,9 +7,31 @@ extends Container
 func _ready():
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
-	pass
+	var front = get_node("Front")
+	var back = get_node("Back")
+	back.hide()
+	front.show()
+	
 
 #func _process(delta):
 #	# Called every frame. Delta is time since last frame.
 #	# Update game logic here.
 #	pass
+
+
+func _on_Button_pressed():
+#	var tween= get_node("Tween")
+#	tween.interpolate_property(self, "rect_position",
+#                get_rect().position, get_rect().position + Vector2(100, 0), 1,
+#                Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+#	tween.start()
+	var front = get_node("Front")
+	var back = get_node("Back")
+	if front. is_visible_in_tree():
+		back.show()
+		front.hide()
+	else:
+		back.hide()
+		front.show()
+	
+	
